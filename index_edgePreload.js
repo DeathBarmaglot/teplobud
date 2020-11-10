48dcdf2bfd452e96304c4cf13e7fc0baea7b86e0
Pasha <54247708+DeathBarmaglot@users.noreply.github.com>
2020-11-10 16:51:13 +0200

    Update index_edgePreload.js

--- index_edgePreload.js
+++ index_edgePreload.js
@@ -52,20 +52,14 @@ if(document.addEventListener ){
 
 
 
+
+
    aLoader = [
-      { load: "https://raw.githubusercontent.com/DeathBarmaglot/teplobud/main/edge_includes/edge.4.0.0.min.js"},
-      { load: "https://raw.githubusercontent.com/DeathBarmaglot/teplobud/main/index_edge.js"},
-      { load: "https://raw.githubusercontent.com/DeathBarmaglot/teplobud/main/index_edgeActions.js"},
-];
-
-
-
-//    aLoader = [
-//       { load: "edge_includes/jquery-2.0.3.min.js"},
-//       { load: "edge_includes/edge.4.0.0.min.js"},
-//       { load: "index_edge.js"},
-//       { load: "index_edgeActions.js"},
-// ];
+       { load: "edge_includes/jquery-2.0.3.min.js"},
+       { load: "edge_includes/edge.4.0.0.min.js"},
+       { load: "index_edge.js"},
+       { load: "index_edgeActions.js"},
+ ];
 
 
 if (AdobeEdge.bootstrapLoading) { signaledLoading = true; AdobeEdge.loadResources=doLoadResources; AdobeEdge.playWhenReady=playWhenReady; }
