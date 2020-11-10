d41680742f7a05507afbbefca6a1676de7fc1cbe
Pasha <54247708+DeathBarmaglot@users.noreply.github.com>
2020-11-10 16:56:16 +0200

    Add files via upload

--- index_edge.js
+++ index_edge.js
@@ -3,7 +3,7 @@
  */
 (function($, Edge, compId){
 //images folder
-var im='images/';
+var im='https://raw.githubusercontent.com/DeathBarmaglot/teplobud/main/images/';
 
 var fonts = {};
 var opts = {
--- index_edgePreload.js
+++ index_edgePreload.js
@@ -54,7 +54,7 @@ if(document.addEventListener ){
 
 
 
-   aLoader = [
+    aLoader = [
        { load: "edge_includes/jquery-2.0.3.min.js"},
        { load: "edge_includes/edge.4.0.0.min.js"},
        { load: "index_edge.js"},
